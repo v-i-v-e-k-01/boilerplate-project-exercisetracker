38412b4928df6ffb96c1d8b76e3bdd1b5d054e33
Vivek D <91778440+v-i-v-e-k-01@users.noreply.github.com>
2024-04-16 22:31:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -121,9 +121,10 @@ app.post("/api/users/:_id/exercises",async function(req,res,next){
 
 app.get("/api/users/:_id/logs", async (req, res, next)=>{
   try{
+
     const user = await User.findById(req.params._id);
     const from = new Date(req.query.from) || new Date(0);
-    const to = new Date( req.query.from)  || new Date();
+    const to = new Date( req.query.to)  || new Date();
     const limit = Number(req.query.limit) || 0;
     if(!user)
     {
